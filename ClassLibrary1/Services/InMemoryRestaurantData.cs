59fb6d261020a61793719f3ca829824fa5abab5a
Paulina Carolina <codelikeamexican@gmail.com>
2019-09-26 08:03:56 -0700

    adding the controller part of the project

---
+++ ClassLibrary1/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using ClassLibrary1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassLibrary1.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{ Id = 1, Name = "spaggethi", Cuisine = CuisineType.Italian},
+                new Restaurant{ Id = 2, Name = "curry ", Cuisine = CuisineType.Indian},
+                new Restaurant{ Id = 1, Name = "tacos", Cuisine = CuisineType.Mexican},
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
+
+z
--- OldToFoodWeb/Controllers/HomeController.cs
+++ OldToFoodWeb/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary1.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,15 @@ namespace OldToFoodWeb.Controllers
 {
     public class HomeController : Controller
     {
+        IRestaurantData db; 
+        public HomeController()
+        {
+            db = new InMemoryRestaurantData();
+        }
         public ActionResult Index()
         {
-            return View();
+            var model = db.GetAll();
+            return View(model);
         }
 
         public ActionResult About()
