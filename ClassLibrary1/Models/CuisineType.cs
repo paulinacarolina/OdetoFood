378933ed8495d114f3b1d6d2b744aec394efa8c6
Paulina Carolina <codelikeamexican@gmail.com>
2019-09-25 21:18:55 -0700

    Adding the data project

---
+++ ClassLibrary1/Models/CuisineType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Models
+{
+    public enum CuisineType
+    {
+        None,
+        Italian,
+        Indian,
+        Mexican
+    }
+}
---
+++ ClassLibrary1/Models/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibrary1.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public CuisineType  Cuisine { get; set; }
+    }
+}
