eb04c749edd8eced156709f64bfb96095f6ec187
Paulina Carolina <codelikeamexican@gmail.com>
2019-09-28 10:49:25 -0700

    adding the controllers config

---
+++ OldToFoodWeb/App_Start/ContainerConfig.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OldToFoodWeb.App_Start
+{
+    public class ContainerConfig
+    {
+        internal static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication))
+        }
+    }
+}
---
+++ OldToFoodWeb/Controllers/GreetingsController.cs
@@ -0,0 +1,23 @@
+﻿using OldToFoodWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OldToFoodWeb.Controllers
+{
+    public class GreetingsController : Controller
+    {
+        // GET: Greetings
+        public ActionResult Index()
+        {
+            var model = new GreetingViewModel();
+            model.Message = ConfigurationManager.AppSettings["message"];
+
+            return View(model);
+            
+        }
+    }
+}
