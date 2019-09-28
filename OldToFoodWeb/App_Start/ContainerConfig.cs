a634f38e315b060158dc3d50a01427308bd04f47
Paulina Carolina <codelikeamexican@gmail.com>
2019-09-28 15:46:10 -0700

    config Autofact container and add first Web Api

--- OldToFoodWeb/App_Start/ContainerConfig.cs
+++ OldToFoodWeb/App_Start/ContainerConfig.cs
@@ -1,16 +1,25 @@
 ﻿using Autofac;
 using Autofac.Integration.Mvc;
+using ClassLibrary1.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace OldToFoodWeb.App_Start
+namespace OldToFoodWeb
+    //method envoke
 {
     public class ContainerConfig
     {
         internal static void RegisterContainer()
         {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<InMemoryRestaurantData>()
+                .As<IRestaurantData>()
+                .SingleInstance();
+            var container = builder.Build();
+            AutofacDependencyResolver.SetAutofacDependencyResolverAccessor(new AutofacDependencyResolver(container));
 
         }
     }
